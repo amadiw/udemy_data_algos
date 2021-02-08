73daf3a340d9fd4ce96ef0d370aa81b04e917300
Amadi Someswaran <amadi@someswaran.com>
2021-02-07 22:24:24 -0500

    completed 16.missingNum.js

--- algodaily/16.missingNum.js
+++ algodaily/16.missingNum.js
@@ -2,8 +2,13 @@ function missingNumbers(numArr) {
   let missing = [];
   for (let i = 1; i < numArr.length; i++) {
     if (numArr[i] !== numArr[i-1]+1) {
-      let difference = numArr[i] - numArr[i -1]
-      missing.push(numArr[i]+1)
+      let difference = numArr[i] - numArr[i -1] //calculates gap
+      let accDif = 1 //used to track gap. increases with each iteration
+      while (accDif < difference) {
+
+        missing.push(numArr[i -1]+accDif)
+        accDif++
+      }
     }
   }
   return missing;
