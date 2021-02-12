5ba5e66be6923b6bf93ddb1c20468cf7a6b323ac
Amadi Someswaran <amadi@someswaran.com>
2021-02-11 19:34:56 -0500

    18.sumPrimes

---
+++ algodaily/18.sumPrimes.js
@@ -0,0 +1,24 @@
+function sumOfPrimes(n) {
+  // function to check if num is prime
+
+  const isPrime = n => {
+    for (let j = 2; j <= Math.sqrt(n); j++) {
+     if (n % j === 0) {
+       return false
+       }
+     }
+      return true
+    }
+
+  let sum = 0
+  for (let i = 2; i <=n; i++) {
+    if (isPrime(i)) {
+      sum += i
+    }
+  }
+  return sum;
+}
+
+console.log(sumOfPrimes(2));
+console.log(sumOfPrimes(10));
+
