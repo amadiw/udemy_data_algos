8bdaf8f60ff2a9352bb0d4814ac9338577ba0090
Amadi Someswaran <amadi@someswaran.com>
2021-03-02 18:24:25 -0500

    set  added

--- udemy/19. Linked Lists/linkedlist.js
+++ udemy/19. Linked Lists/linkedlist.js
@@ -43,46 +43,59 @@ class LinkedList {
       this.head = null;
       this.tail = null;
     }
-    return curr
+    return curr;
   }
 
-  shift() { //remove from front ie head
-    if (!this.head) return undefined
+  shift() {
+    //remove from front ie head
+    if (!this.head) return undefined;
 
-    let curr = this.head
-    this.head = curr.next
-    this.length--
-    if (this.length === 0) {//if nothing left, make tail null
-      this.tail = null
+    let curr = this.head;
+    this.head = curr.next;
+    this.length--;
+    if (this.length === 0) {
+      //if nothing left, make tail null
+      this.tail = null;
     }
-    return curr
+    return curr;
   }
 
-  unshift(val) { //adds to front of list, head
-    let newNode = new Node(val)
-    let curr = this.head
+  unshift(val) {
+    //adds to front of list, head
+    let newNode = new Node(val);
+    let curr = this.head;
 
-    if(!this.head) { //if list empty, add newNode as head
-      this.head = newNode
+    if (!this.head) {
+      //if list empty, add newNode as head
+      this.head = newNode;
     } else {
-      newNode.next = curr //makes old head preceed new head
-      this.head = newNode
+      newNode.next = curr; //makes old head preceed new head
+      this.head = newNode;
     }
-    this.length++
-    return this
+    this.length++;
+    return this;
   }
 
   get(idx) {
-    if (idx < 0 || idx >= this.length) return undefined
+    if (idx < 0 || idx >= this.length) return undefined;
 
-    let count = 0
-    let curr = this.head
+    let count = 0;
+    let curr = this.head;
 
     while (count < idx) {
-      curr = curr.next
-      count++
+      curr = curr.next;
+      count++;
     }
-    return curr.val
+    return curr;
+  }
+
+  set(val, idx) {
+    let found = this.get(idx);
+    if (!found) return false;
+    // console.log(val);
+    // console.log("found: ", found);
+    found.val = val;
+    return found;
   }
 }
 
@@ -90,16 +103,19 @@ let list = new LinkedList();
 console.log(list.push(2));
 console.log(list.push(4));
 console.log(list.push(6));
-console.log(list.pop())
-console.log(list)
-console.log(list.shift())
-console.log(list)
+console.log(list.pop());
+// console.log(list);
+console.log(list.shift());
+// console.log(list);
 console.log(list.push(5));
 console.log(list.push(6));
+// console.log(list);
+console.log(list.unshift(7));
+// console.log(list);
+// console.log(list.get(0));
+// console.log(list.get(1));
+// console.log(list.get(2));
+// console.log(list.get(3));
+// console.log(list.get(4));
+console.log(list.set(69, 3));
 console.log(list)
-console.log(list.unshift(7))
-console.log(list)
-console.log(list.get(0))
-console.log(list.get(1))
-console.log(list.get(2))
-console.log(list.get(3))
