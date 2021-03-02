9acc6eebe0c7569865aa3822b94c6e34c5685950
Amadi Someswaran <amadi@someswaran.com>
2021-03-02 17:11:09 -0500

    pop added

--- udemy/19. Linked Lists/linkedlist.js
+++ udemy/19. Linked Lists/linkedlist.js
@@ -1,33 +1,55 @@
 class Node {
   constructor(val) {
-    this.val = val
-    this.next = null
+    this.val = val;
+    this.next = null;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.head = null
-    this.tail = null
-    this.length = 0
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   push(val) {
-    let newNode = new Node(val)
+    let newNode = new Node(val);
 
-    if(!this.head) {
-      this.head = newNode
-      this.tail = this.head
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
     } else {
-      this.tail.next = newNode
-      this.tail = newNode
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.length++
-    return this
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let curr = this.head;
+    let prev = curr;
+
+    while (curr.next) {
+      prev = curr;
+      curr = curr.next;
+    }
+    this.tail = prev; //makes 2nd to last new tail
+    this.tail.next = null; //breaks link to old tail
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return curr
   }
 }
 
-let list = new LinkedList()
-console.log(list.push(2))
-console.log(list.push(4))
-console.log(list.push(6))
+let list = new LinkedList();
+console.log(list.push(2));
+console.log(list.push(4));
+console.log(list.push(6));
+console.log(list.pop())
+console.log(list)
