bcc367c26910c255411b3d205fd39dc79f9b548c
Amadi Someswaran <amadi@someswaran.com>
2021-03-25 12:24:14 -0400

    new file

---
+++ Algos/Misc/wordSearch_LC.js
@@ -0,0 +1,13 @@
+const wordPattern = (pattern, string) => {
+
+
+
+
+
+
+
+  return true
+}
+
+console.log(wordPattern('adca', 'cat dog dog cat')) //false
+console.log(wordPattern('adca', 'cat dog dog cat')) //false
