1160ad07b37539207d216e155271c513c8813417
Amadi Someswaran <amadi@someswaran.com>
2021-03-26 17:16:53 -0400

    isPalindrome

--- Recursion/productArray.js
+++ Recursion/productArray.js
@@ -9,3 +9,11 @@ const productArray = (arr) => {
 
 console.log(productArray([1,2,3,10]))//60
 console.log(productArray([1,2,10,10]))//200
+
+const productOfArray = (arr) => {
+
+  if (arr.length === 1) return arr[0]
+  return arr[0] * productOfArray(arr.slice(1))
+}
+
+console.log(productOfArray([1,2,10,10]))//200
--- Recursion/recursiveRange.js
+++ Recursion/recursiveRange.js
@@ -1,7 +1,7 @@
 function recursiveRange(num){
 if (num === 0) return num
 
-return num += recursiveRange(num - 1)
+return num + recursiveRange(num - 1)
 
 }
 
