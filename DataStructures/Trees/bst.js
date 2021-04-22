0a97b557a3b73acbff5a2644ac4521f7bcb90a03
Amadi Someswaran <amadi@someswaran.com>
2021-04-22 10:20:41 -0400

    cleanup

--- DataStructures/Trees/bst.js
+++ DataStructures/Trees/bst.js
@@ -36,7 +36,47 @@ class BST {
       }
     }
   }
-  find(val) {
+
+  insertRecursive(val) {
+    let node = new Node(val)
+
+    const search = curr => {
+      if(val < curr.value) {
+        if(!curr.left) {
+          curr.left = node
+        } else {
+          search((curr.left))
+        }
+      } else if(val > curr.value) {
+        if(!curr.right) {
+          curr.right = node
+        } else {
+          search(curr.right)
+        }
+      }
+    }
+    search(this.root)
+  }
+
+  min() {
+    let curr = this.root
+
+    while(curr.left) {
+      curr = curr.left
+    }
+    return curr.value
+  }
+
+  max(){
+    let curr = this.root
+
+    while(curr.right) {
+      curr = curr.right
+    }
+    return curr.value
+  }
+
+  contains(val) {
     if(!this.root) return false
     let curr = this.root
     let found = false
@@ -68,10 +108,54 @@ class BST {
     }
     return results
   }
+  dfsPre() {
+    const results = []
+    let node = this.root
 
+    const traverse = node => {
+      results.push(node.value)
+      if(node.left) traverse(node.left)
+      if(node.right) traverse(node.right)
+    }
+    traverse(node)
+    return results
+  }
+
+  dfsPost(){
+    const results = []
+    let node = this.root
+    const traverse = node => {
+      if(node.left) traverse(node.left)
+      if(node.right) traverse(node.right)
+      results.push(node.value)
+    }
+    traverse(node)
+    return results
+  }
+
+  dfsInOrder(){
+    const results = []
+    let node = this.root
+
+    const traverse = node => {
+      if(node.left) traverse(node.left)
+      results.push(node.value)
+      if(node.right) traverse(node.right)
+    }
+    traverse(node)
+    return results
+  }
 
 }
 
+
+
+  /*
+   10
+  /  \
+ 6   15
+3  8   20
+  */
 const tree = new BST();
 tree.insert(10);
 tree.insert(6);
@@ -79,5 +163,11 @@ tree.insert(15);
 tree.insert(3)
 tree.insert(8)
 tree.insert(20)
-console.log(tree.bfs()); //[10,6,15,3,8,20]
-// console.log(JSON.stringify(tree));
+// console.log(tree.bfs()); //[10,6,15,3,8,20]
+// console.log(tree.dfsPre()) //[10,6,3,8,15,20]
+// console.log(tree.dfsPost()) //[3,8,6,20,15,10]
+console.log(tree.dfsInOrder()) //[3,6,8,10,15,20 ]
+
+// tree.insertRecursive(50)
+// console.log(tree.min())
+// console.log(JSON.stringify(tree))
